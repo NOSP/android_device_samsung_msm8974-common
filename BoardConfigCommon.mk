0889fa5a1b791b50fb4e9d70500a0122694b0b3e
Chirayu Desai <chirayudesai1@gmail.com>
2014-11-27 20:19:14 +0200

    msm8974-common: SELinux: include policy from qcom sepolicy repo
    Change-Id: I64d469266aade418be1b22cc82739ce6c1254fb9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -57,3 +57,6 @@ EXTENDED_FONT_FOOTPRINT := true
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 BOARD_VOLD_MAX_PARTITIONS := 28
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+
+# SELinux
+include device/qcom/sepolicy/sepolicy.mk
